8c60086faf959b6bc349ba212f779f77a49cd257
Dpbm <dpbm136@gmail.com>
2025-02-16 00:08:47 -0300

    updated backends

--- aer_plugin/plugin.py
+++ aer_plugin/plugin.py
@@ -1,3 +1,4 @@
+import os
 from .interface import (
     PluginInterface,
     Backend,
@@ -20,8 +21,13 @@ class Plugin(PluginInterface):
     """
 
     def __init__(self):
-        backends = ["aer"]
-        super().__init__(backends)
+        current_file_path = os.path.dirname(__file__)
+        print(current_file_path)
+        backends_relative_path = os.path.join(current_file_path, "backends.txt")
+
+        with open(backends_relative_path, "r", encoding="UTF-8") as file:
+            backends = list(map(lambda x: x.replace("\n", ""), file))
+            super().__init__(backends)
 
     @check_backend
     @check_result_type
