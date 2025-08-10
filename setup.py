5b5a459bad27ea4e0ee5cf1f8c060b276a5acd92
Dpbm <dpbm136@gmail.com>
2025-08-10 13:07:47 -0300

    update aer version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def get_long_description():
 
 setup(
     name="aer_plugin",
-    version="0.0.1",
+    version="0.0.2",
     install_requires=get_requirements(),
     packages=find_packages(exclude=["tests"]),
     classifiers=[
